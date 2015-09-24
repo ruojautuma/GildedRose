c14b116d6483919c579b5cecc6137225c213f2b8
ruojautuma <ruojautuma@gmail.com>
2015-09-24 13:34:53 +0300

    Fix initial quality in ConjuredItem

--- src/fi/oulu/tol/sqat/ConjuredItem.java
+++ src/fi/oulu/tol/sqat/ConjuredItem.java
@@ -4,6 +4,7 @@ public final class ConjuredItem extends Item {
 
 	public ConjuredItem(String name, int sellIn, int quality) {
 		super(name, sellIn, quality);
+		this.quality = quality;
 	}
 
 	@Override
